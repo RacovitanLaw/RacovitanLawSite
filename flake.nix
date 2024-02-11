304caaaa2792ec5414155d8bb1d500cb852ef219
Brandon Barker <brandon.barker@gmail.com>
2024-02-11 11:42:40 -0500

    initial commit

---
+++ flake.nix
@@ -0,0 +1,27 @@
+# in flake.nix
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.11";
+    flake-utils.url = "github:numtide/flake-utils";
+    # rust-overlay.url = "github:oxalica/rust-overlay";
+  };
+  outputs = { self, nixpkgs, flake-utils }: # rust-overlay
+    flake-utils.lib.eachDefaultSystem
+      (system:
+        let
+          # overlays = [ (import rust-overlay) ];
+          pkgs = import nixpkgs {
+            inherit system; # overlays;
+          };
+        in
+        with pkgs;
+        {
+          devShells.default = mkShell {
+            buildInputs = [ 
+              zola
+              # yarn # https://www.printlnhello.world/blog/zola-tailwindcss/ 
+            ];
+          };
+        }
+      );
+}
